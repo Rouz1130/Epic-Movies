103e03946f2ca08a2339e41229ee9b6ad97dd97f
rouz majlessi <rmajlessi@gmail.com>
2017-03-07 00:54:39 -0800

    fix navbar too inverse

--- app/routes/theaters.js
+++ app/routes/theaters.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
     return Ember.RSVP.hash({
       get theater() {
 
-        var url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-11-10&api_key=tgq7sdn5xsx2zr3ccp9344yf&zip=98104';
+        var url = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-11-10&api_key=2eduftsxuau4jv4mj2v7nqx5';
 
         return Ember.$.getJSON(url).then(function(responseJSON) {
           console.log(responseJSON);
